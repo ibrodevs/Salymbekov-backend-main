f3613e40f9ea15c5744a3aa147f903ff9e1eced1
IbroIT <ibroabdraimov20@gmail.com>
2025-12-27 13:06:21 +0600

    Add production frontend URL to CORS settings

--- core/settings.py
+++ core/settings.py
@@ -118,6 +118,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://localhost:5173",
+    "https://127.0.0.1:5173",
+    "https://localhost:3000",
+    "https://127.0.0.1:3000",
+    "https://salymbekov.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True

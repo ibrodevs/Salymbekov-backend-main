bf58dae0289cd1cfee622faec078ded95dc24526
Adilhan <adilhansatymkulov40@gmail.com>
2026-01-25 12:02:44 +0600

    hello

--- academic_council/models.py
+++ academic_council/models.py
@@ -6,7 +6,10 @@ class AcademicCouncil(models.Model):
     name_en = models.CharField(max_length=255, verbose_name='ФИО на английском')
     name_kg = models.CharField(max_length=255, verbose_name='ФИО на кыргызском')
 
-    role = models.CharField(max_length=255, null=True, blank=True)
+    role_ru = models.CharField(max_length=255, null=True, blank=True, verbose_name='role(ru)')
+    role_en = models.CharField(max_length=255, null=True, blank=True, verbose_name='role(en)')
+    role_kg = models.CharField(max_length=255, null=True, blank=True, verbose_name='role(kg)')
+
     photo = models.ImageField(upload_to='acadmic_council_photos/')
 
     def __str__(self):
@@ -15,6 +18,9 @@ class AcademicCouncil(models.Model):
     def get_name(self, lang='ru'):
         return getattr(self, f'name_{lang}', self.name_ru)
     
+    def get_role(self,lang='ru'):
+        return getattr(self,f'role_{lang}', self.role_ru)
+
     class Meta:
         verbose_name = 'Член академического совета'
         verbose_name_plural = 'Члены академического совета'
--- academic_council/serializers.py
+++ academic_council/serializers.py
@@ -3,12 +3,17 @@ from rest_framework import serializers
 
 class AcademicCouncilSerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField()
+    role = serializers.SerializerMethodField()
     
     class Meta:
         model = AcademicCouncil
-        fields = 'name, photo, role'
+        fields = ['name, photo, role']
 
     def get_name(self, obj):
         lang = self.context.get('lang', 'ru')
         return obj.get_name(lang=lang)
+
+    def get_role(self, obj):
+        lang = self.context.get('lang', 'ru')
+        return obj.get_role(lang=lang)
 

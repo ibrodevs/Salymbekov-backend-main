3dcb2e0b14bbf44d4652ee5c8e8698aaf886bcb6
Adilhan <adilhansatymkulov40@gmail.com>
2026-01-30 20:42:30 +0600

    commit

--- partners/urls.py
+++ partners/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import PartnerListAPIView
 
 urlpatterns = [
-	path('partners', PartnerListAPIView.as_view(), name='partners-list')
+	path('partners/', PartnerListAPIView.as_view(), name='partners-list')
 ]

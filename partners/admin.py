31be51da75696f9b1e270416d8a2778a1e91bc83
Ibro <Apple@MacBook-Air.local>
2026-01-31 16:07:32 +0600

    sdfs

--- partners/admin.py
+++ partners/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+from .models import Partner
 
-# Register your models here.
+
+@admin.register(Partner)
+class PartnerAdmin(ModelAdmin):
+    list_display = ['id', 'name_ru', 'name_en', 'name_kg']
+    list_display_links = ['id', 'name_ru']
+    search_fields = ['name_ru', 'name_en', 'name_kg']
+    list_filter = ['name_ru']
+    fields = [
+        'logo',
+        'name_ru', 'name_en', 'name_kg',
+        'description_ru', 'description_en', 'description_kg',
+        'coord1', 'coord2'
+    ]
